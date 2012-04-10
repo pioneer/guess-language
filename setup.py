b2ac718c10cbf6c6f92928e351071e6eb1b8183a
Serge Tarkovski <serge.tarkovski@gmail.com>
2012-04-10 18:35:58 +0300

    Added version higher than in official repository

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='guess-language',
-    version = '0.2',
+    version = '0.3',
     description = 'Guess the natural language of a text',
     license = 'LGPL',
     platforms = ['any'],
